bfc928a54b7e30dc454957537da19806ab7a4c51
endrearato <endre.arato.81@gmail.com>
2022-01-02 21:50:19 +0100

    in_progress

--- js-2-feladatok-calculator/calc.js
+++ js-2-feladatok-calculator/calc.js
@@ -1,5 +1,25 @@
 
-function onDisplay (num) {
-    document.form.textview.value = document.form.textview.value + num    
-} 
-onDisplay()
+const display = document.querySelector('.textview')
+const allNumber = document.querySelectorAll('.num')
+const allOperator = document.querySelectorAll('.oper')
+const equal = document.querySelector('.equal')
+const del = document.querySelector('.del')
+
+let temp = '';
+let operator = '';
+
+allNumber.forEach(number=>{
+    number.addEventListener('click', (e) =>{
+        temp += e.target.innerText
+        //console.log(temp)
+        display.value = temp
+    })
+})
+
+allOperator.forEach(oper=>{
+    oper.addEventListener('click', (e)=>{
+        operator += e.target.innerText
+        display.value = temp + operator
+    })
+})
+

0cea370e9092f079dc770882065ed7c1e3ec191a
endrearato <endre.arato.81@gmail.com>
2022-01-02 18:00:25 +0100

    done

---
+++ scss-feladatok-sitebuilding/bootstrap-sitebuilding-project/main.js
@@ -0,0 +1,29 @@
+const modal = document.getElementById("modal");
+
+const myModalButton1 = document.getElementById("myModalButton1");
+const myModalButton2 = document.getElementById("myModalButton2");
+const myModalButton3 = document.getElementById("myModalButton3");
+
+const button1 = document.getElementById("okButton");
+const button2 = document.getElementById("cancelButton");
+
+
+myModalButton1.onclick = function() {
+  modal.style.display = "block";
+}
+
+myModalButton2.onclick = function() {
+  modal.style.display = "block";
+}
+
+myModalButton3.onclick = function() {
+  modal.style.display = "block";
+}
+
+button1.onclick = function() {
+    modal.style.display = "none";
+}
+
+button2.onclick = function() {
+    modal.style.display = "none";
+}
